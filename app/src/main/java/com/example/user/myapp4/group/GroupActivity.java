5668d2ed2d30adfbad247d85caf436dddea4dc37
HAN JI WON <hjw8420@gmail.com>
2016-06-18 11:04:36 +0900

    4교시

---
+++ app/src/main/java/com/example/user/myapp4/group/GroupActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.myapp4.group;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.user.myapp4.R;
+
+public class GroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+    }
+}
